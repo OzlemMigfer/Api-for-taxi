e250f8b363c8070efc8c967e0da4b4a576ebd7ff
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-05-20 12:59:10 +0300

    add the db table,columns
    add the db table,columns cities-districts-neighbourhoods

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/cities', [CityController::class, 'index']);
+Route::get('/cities/{city}', [CityController::class, 'show']);
